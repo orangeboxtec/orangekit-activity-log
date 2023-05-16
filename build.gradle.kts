b281113b18edf575e681861fafa13fff7c030bef
Diego Mondego <diego@orangebox.technology>
2023-05-16 07:50:21 -0300

    ActivityLogBuilder

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.orangebox.kit.activitylog"
-version = "1.0.6"
+version = "1.0.7"
 
 repositories {
     mavenCentral()
@@ -30,7 +30,7 @@ dependencies {
     implementation("io.quarkus:quarkus-resteasy-reactive")
     implementation("org.mongodb:bson:4.9.1")
 
-    implementation("com.orangebox.kit.core:orangekit-core:1.0.9")
+    implementation("com.orangebox.kit.core:orangekit-core:1.0.10")
     implementation("com.orangebox.kit.authkey:orangekit-authkey:1.0.1")
     implementation("com.orangebox.kit.notification:orangekit-notification:1.0.3")
 
